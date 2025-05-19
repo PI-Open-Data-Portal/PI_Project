f55509807a324e81441f329d8be11518f943122b
costinha03 <dmcosta03@ua.pt>
2025-05-19 01:16:05 +0100

    Login

---
+++ Backend/api/src/main/java/opendata/portal/api/model/AuthRequest.java
@@ -0,0 +1,26 @@
+package opendata.portal.api.model;
+
+
+public class AuthRequest {
+
+    private String password;
+    private String email;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
